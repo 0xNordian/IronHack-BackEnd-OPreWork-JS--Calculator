2724b39503b0b09fa66c3a8de9a387a400d92591
0xNordian <nestor86@gmail.com>
2023-01-02 17:44:20 +0100

    Simple Calculation Logic Done. I didn't implement MDAS or PEMDAS model. Pending to show the result on the screen

--- app.js
+++ app.js
@@ -1,112 +1,60 @@
-let arr = []
+let arr = [];
+let tempCalc = [];
+let tempFunction = [];
+let storedCalc = 0;
+let finalResult = [];
+
+let numbers = [];
+let operators = [];
 
 const calcBtn = document.querySelectorAll(".div-btn button");
+
+function getResult(arr) {
+  let result = arr[0];
+  for (let i = 1; i < arr.length; i += 2) {
+    if (arr[i] === "+") {
+      result += arr[i + 1];
+    } else if (arr[i] === "-") {
+      result -= arr[i + 1];
+    } else if (arr[i] === "*") {
+      result *= arr[i + 1];
+    } else if (arr[i] === "/") {
+      result /= arr[i + 1];
+    }
+  }
+  return result;
+}
+
 calcBtn.forEach((button) => {
-  console.log("allButton: ", button);
   button.addEventListener("click", function () {
-    arr.push(this.textContent);
-    console.log("arr: ", arr)
-    return arr
+    if (["+", "-", "*", "/"].includes(button.textContent)) {
+      operators.push(button.textContent);
+    } else if (/^\d+$/.test(button.textContent)) {
+      numbers.push(parseInt(button.textContent));
+    }
+
+    switch (button.textContent) {
+      case "=":
+        tempCalc.push(Number(numbers.join("")));
+        for (i = 0; i < tempCalc.length; i++) {
+          tempFunction.push(tempCalc[i], operators[i]);
+          // console.log(`TEST${i}: `, tempCalc[i], operators[i])
+        }
+        tempFunction.pop();
+        finalResult = getResult(tempFunction);
+        console.log("finalResult: ", finalResult);
+        break;
+      case "+":
+      case "-":
+      case "*":
+      case "/":
+        tempCalc.push(Number(numbers.join("")));
+        numbers = [];
+        break;
+    }
+    console.log("tempFunction: ", tempFunction);
+    console.log("storedCalc: ", storedCalc);
+
+    return arr;
   });
 });
-
-// function calculate(arr) {
-//     let result = 0;
-//     let currentNumber = '';
-//     let currentOperator = '';
-  
-//     for (let i = 0; i < arr.length; i++) {
-//       const input = arr[i];
-//       if (typeof input === 'number') {
-//         currentNumber += input;
-//       } else if (typeof input === 'string') {
-//         if (!currentOperator) {
-//           result = Number(currentNumber);
-//           currentNumber = '';
-//         } else {
-//           result = calculate(result, Number(currentNumber), currentOperator);
-//           currentNumber = '';
-//         }
-//         currentOperator = input;
-//       }
-//     }
-  
-//     if (currentNumber) {
-//       result = calculate(result, Number(currentNumber), currentOperator);
-//     }
-  
-//     return result;
-//   }
-
-function calculate(input) {
-    // use a regular expression to match all the numbers and operators in the input string
-    const tokens = input.match(/\d+|\+|-|\*|\/|\^|%/g);
-  
-    // initialize the list of numbers and operators
-    const numbers = [];
-    const operators = [];
-  
-    // iterate through the tokens
-    for (const token of tokens) {
-      // if the token is a number, add it to the list of numbers
-      if (/\d+/.test(token)) {
-        numbers.push(parseInt(token, 10));
-      }
-      // if the token is an operator, add it to the list of operators
-      else {
-        operators.push(token);
-      }
-    }
-  
-    // initialize the result to the first number
-    let result = numbers[0];
-  
-    // iterate through the list of numbers and operators, starting at index 0
-    for (let i = 0; i < numbers.length - 1; i++) {
-      // get the current number and operator
-      const number = numbers[i + 1];
-      const operator = operators[i];
-  
-      // handle exponentiation and modulo first, since they have higher precedence than other operators
-      if (operator === '^' || operator === '%') {
-        if (operator === '^') {
-          result = Math.pow(result, number);
-        } else if (operator === '%') {
-          result = result % number;
-        }
-      }
-      // handle multiplication and division next, since they have higher precedence than addition and subtraction
-      else if (operator === '*' || operator === '/') {
-        if (operator === '*') {
-          result *= number;
-        } else if (operator === '/') {
-          result /= number;
-        }
-      }
-      // handle addition and subtraction last
-      else if (operator === '+' || operator === '-') {
-        if (operator === '+') {
-          result += number;
-        } else if (operator === '-') {
-          result -= number;
-        }
-      }
-    }
-  
-    return result;
-  }
-  
-  // test the calculator
-  console.log(calculate('1 + 2 * 3')); // 7
-  console.log(calculate('1 + 2 * 3 - 4 / 2')); // 5
-  console.log(calculate('2 ^ 3 ^ 2')); // 512
-  console.log(calculate('10 % 3 * 5')); // 5
-  
-  function showDisplay(event) {
-    const x = event.target.innerText;
-    if (displayBox.innerHTML == 0) {
-      return (displayBox.innerHTML = x);
-    }
-    console.log("display: ", displayBox.innerHTML += x);
-    return (displayBox.innerHTML += x);
-  }
